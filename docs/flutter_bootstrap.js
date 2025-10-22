d8d82bd7854f3f53cad71732ebfed6e131a3eefc
Jonathan Woodbury <jpwoodbu@gmail.com>
2025-10-22 03:50:33 -0400

    Release 0.1.12

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,10 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
+<<<<<<< Updated upstream
     serviceWorkerVersion: "4280440968"
+=======
+    serviceWorkerVersion: "2503355807"
+>>>>>>> Stashed changes
   }
 });
